80d8d0e7a8e868374f98264d39dac7e517fd3ac1
Davsua <91037586+Davsua@users.noreply.github.com>
2021-10-01 15:38:59 -0500

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,14 +18,14 @@ int main(void)
 	{
 		printf("less than 6 and not 0\n");
 	}
-	else if (n % 10 == 0)
+	else if (n % 10 > 5))
 	{
 		printf("0\n");
 	}
-	else if (n % 10 > 5)
-
+	else 
+	{
 		 printf("greater than 5\n");
-
+	}
 
 	return (0);
 }
