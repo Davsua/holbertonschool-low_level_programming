3f00a62515b237b64d9f6c85fd8692c89ce92df5
Davsua <davidsuarezcarvajal98@gmail.com>
2021-09-30 21:21:14 -0500

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /**stdio.h means studio.header that permit us use variables*/
+
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+
+}
