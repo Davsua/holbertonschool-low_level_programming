e71f0cefdb6587fe498e87482342e80f90175541
Davsua <91037586+Davsua@users.noreply.github.com>
2021-10-01 12:38:42 -0500

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-    if (n > 0){
+    if (n > 0)
+    {
         printf("%d is positive ", n);
     }
-    else{
+    else
+    {
         if (n == 0)
 
         printf("%d is zero ", n);
 
-        else{
+        else
+	{
             printf("%d is negative ", n);
         }
     }
