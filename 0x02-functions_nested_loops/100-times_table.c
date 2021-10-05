7580c9df862d11e0f213f2efd93372f310e5760f
Davsua <91037586+Davsua@users.noreply.github.com>
2021-10-05 17:27:53 -0500

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 	int a, b, c;
 
 	if (n >= 0 && n <= 15)
-	{ 
+	{
 		for (a = 0; a <= n; a++)
 		{
 			for (b = 0; b <= n; b++)
@@ -42,8 +42,8 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((c / 100) + '0');
 					_putchar(((c / 10) % 10) + '0');
-					_putchar((c % 10) + '0');	
-				} 
+					_putchar((c % 10) + '0');
+				}
 			}
 			_putchar('\n');
 		}
