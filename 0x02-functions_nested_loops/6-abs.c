e53ba3a9a5109951aa331895844ada6e96e09cc7
Davsua <davidsuarezcarvajal98@gmail.com>
2021-10-05 11:33:53 -0500

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - checks for absolute values
+ * @n: the character to check
+ * Return: n
+ */
+
+int _abs(int n)
+{
+
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+
+
+}
