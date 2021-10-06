e351302de72e53872f3255b1460e0945d9100c80
Davsua <davidsuarezcarvajal98@gmail.com>
2021-10-06 00:11:09 -0500

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+  * main - Prints the first 52 fibonacci numbers
+  *
+  * Return: Nothing!
+  */
+
+int main(void)
+{
+	int i;
+	long j = 1, k = 2;
+
+	for (i = 0; i < 50; i++)
+	{
+		if (i == 0)
+		{
+			printf("%ld", j);
+		}
+		else if (i == 1)
+		{
+			printf(", %ld", k);
+		}
+		else
+		{
+			k = k + j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+	}
+}
