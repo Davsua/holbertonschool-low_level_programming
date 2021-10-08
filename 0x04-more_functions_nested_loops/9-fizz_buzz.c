5c6cc49fb255fe546124ce85f258206f54d16f2e
Davsua <davidsuarezcarvajal98@gmail.com>
2021-10-08 12:50:28 -0500

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the numbers between 1-100
+ * except the multiples of 3, multiples of 5
+ * and multiples of both
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	int a, b = 100;
+
+	for (a = 1; a <= b; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("fizzbuzz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("buzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		else
+		{
+		printf("%d ", a);
+		}
+	}
+	return (0);
+}
