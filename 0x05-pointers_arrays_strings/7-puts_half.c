348d874a29ed1b8f3902cd85598c6102103906ae
Davsua <davidsuarezcarvajal98@gmail.com>
2021-10-11 23:09:57 -0500

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints half of a string
+ * @str: string that contains numbers beetwen 0 - 10
+ * return: null
+ */
+
+void puts_half(char *str)
+{
+	int cant = 0;
+	int i, j;
+
+	while (str[cant] != 0)
+	{
+		cant++;
+	}
+	j = cant / 2;
+	for (i = 0; i < j; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
+
